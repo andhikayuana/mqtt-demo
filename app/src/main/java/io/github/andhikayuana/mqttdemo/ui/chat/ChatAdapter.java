3d18954f5112e0860028093d84fcc20c28f7424e
Andhika Yuana <andhikayuana@gmail.com>
2017-08-12 02:35:06 +0700

    remove condition

--- app/src/main/java/io/github/andhikayuana/mqttdemo/ui/chat/ChatAdapter.java
+++ app/src/main/java/io/github/andhikayuana/mqttdemo/ui/chat/ChatAdapter.java
@@ -1,7 +1,6 @@
 package io.github.andhikayuana.mqttdemo.ui.chat;
 
 import android.content.Context;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import io.github.andhikayuana.mqttdemo.R;
-import io.github.andhikayuana.mqttdemo.util.Const;
-import io.github.andhikayuana.mqttdemo.util.SharedPref;
 
 /**
  * @author yuana <andhikayuana@gmail.com>
@@ -76,10 +73,9 @@ public class ChatAdapter extends BaseAdapter {
         }
 
         public void bind(Chat item) {
-            // FIXME: 8/12/17
             tvItemChatMessage.setText(item.getMessage());
-            tvItemChatMessage.setGravity(item.getClientId().equals(SharedPref.getString(Const.KEY.CLIENT_ID)) ?
-                    Gravity.RIGHT : Gravity.LEFT);
+//            tvItemChatMessage.setGravity(item.getClientId().equals(SharedPref.getString(Const.KEY.CLIENT_ID)) ?
+//                    Gravity.RIGHT : Gravity.LEFT);
         }
     }
 }
